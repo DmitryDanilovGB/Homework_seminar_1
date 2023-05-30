b74c1a7f293ebaa42d274bf9c6e647abe2e1513b
dima <dima-danilov96@mail.ru>
2023-05-30 16:15:54 +0700

    Второе задание семинара 2

---
+++ Seminar_2/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿var strnum = "1234";
+
+int num;
+
+bool isnumber = int.TryParse(strnum, out num);
+
+if (isnumber && strnum.Length >= 3 )
+{
+    Console.WriteLine("Третьяя цифра числа: " + strnum[2]);
+}
+
+else
+{
+    Console.WriteLine("Ошибка! Строка должна состоять только из цифр!");
+}
