4e1c28db7e6523cc395cf611f21247e7e73f3b98
dima <dima-danilov96@mail.ru>
2023-05-30 16:19:34 +0700

    Третье задание семинар 2

---
+++ Seminar_2/task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру: ");
+int num = int.Parse(Console.ReadLine());
+while (num <=7)
+{
+
+    if(num == 6 || num == 7)
+    {
+    Console.WriteLine("да");
+    }
+
+    else 
+    {
+    Console.WriteLine("нет");
+    }
+}
+
+Console.WriteLine("Ошибка! Введите число от 1 до 7(количество дней недели)");
